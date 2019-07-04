1360e626e6e656fc230ee658e7c8244376af99ef
SuperC25 <43820420+SuperC25@users.noreply.github.com>
2019-07-04 17:49:08 +0800

    Add files via upload

---
+++ minion.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+rpm -qa | grep salt-minion-2019
+if [ $? -eq 0 ]
+then
+    echo "已经安装salt-minion-2019现在退出"
+    exit 1
+else
+    echo "开始安装"
+fi
+
+
+
+v=`cat /etc/redhat-release|sed -r 's/.* ([0-9]+)\..*/\1/'`
+ 
+if [ $v -eq 6 ]; then 
+    echo "系统版本：Centos 6"
+fi
+ 
+if [ $v -eq 7 ]; then
+    echo "系统版本：Centos 7"
+fi
+
+#获取本机ip地址
+ipaddr=`ifconfig |grep team0 -A 1|grep inet|awk '{print $2}'`
+if [ "$ipaddr" =  "" ]
+then
+    ipaddr=`ifconfig |grep bond0 -A 1|grep inet|awk '{print $2}'|awk -F':' '{print $2}'`
+fi
+if  [ "$ipaddr" =  "" ]
+then
+    ipaddr=`/sbin/ifconfig | grep 'inet ' | awk '{print $2}' | sed -e '/127\.0\.0\.1/d'`
+fi
+echo "服务器IP地址：$ipaddr"
+
+mkdir /etc/yum.repos.d/back-20190626
+mv /etc/yum.repos.d/*.repo /etc/yum.repos.d/back-20190626/
+
+#pip -V
+#if [ $? -eq 0 ]
+#then
+#    echo "执行成功"
+#else
+#    echo "执行失败"
+#    wget -O /etc/yum.repos.d/get-pip.py  http://62.234.76.25:9999/get-pip.py
+#    python /etc/yum.repos.d/get-pip.py
+#    rm -f /etc/yum.repos.d/get-pip.py
+#fi
+
+
+
+if [ $v -eq 6 ];then
+    echo "-------执行Centos6 salt安装-------"
+	yum clean all
+	#wget -O /etc/yum.repos.d/CentOS-Base.repo http://62.234.76.25:9999/centos6.repo
+	#wget -O /etc/yum.repos.d/epel.repo http://62.234.76.25:9999/epel6.repo
+#	yum install -y https://repo.saltstack.com/yum/redhat/salt-repo-latest.el6.noarch.rpm 
+	wget -O /etc/yum.repos.d/salt.repo wget http://resource.haier.net/download/init/salt/salt6.repo
+	wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-6.repo
+	wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-6.repo
+	yum makecache
+	yum install -y salt-minion
+	sed -i 's/^master.*/#&/' /etc/salt/minion
+	sed -i 's/^id.*/#&/' /etc/salt/minion
+	rm -f  /etc/salt/pki/minion/*
+	cat >> /etc/salt/minion << EOF
+master: 10.159.32.200
+id: $ipaddr
+EOF
+	chkconfig salt-minion on
+	echo "重启salt-minion"
+	service salt-minion restart
+fi
+
+if [ $v -eq 7 ];then
+	echo "-------执行Centos7 salt安装-------"
+	yum clean all
+	#wget -O /etc/yum.repos.d/CentOS-Base.repo http://62.234.76.25:9999/centos7.repo
+        #wget -O /etc/yum.repos.d/epel.repo http://62.234.76.25:9999/epel7.repo
+#	yum install -y https://repo.saltstack.com/yum/redhat/salt-repo-latest.el7.noarch.rpm 
+	wget -O /etc/yum.repos.d/salt.repo wget http://resource.haier.net/download/init/salt/salt7.repo
+	wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
+	wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+	yum makecache
+	yum install -y python2-certifi
+	yum install -y salt-minion
+    sed -i 's/^master.*/#&/' /etc/salt/minion
+    sed -i 's/^id.*/#&/' /etc/salt/minion
+    rm -f  /etc/salt/pki/minion/*
+    cat >> /etc/salt/minion << EOF
+master: 10.159.32.200
+id: $ipaddr
+EOF
+	systemctl enable salt-minion
+	echo "重启salt-minion"
+	systemctl restart salt-minion
+fi
+echo "-------Saltstack Minion初始化完成-------"
