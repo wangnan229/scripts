45d53f768ab08314395e3b755bae1366a98d19dd
SuperC25 <43820420+SuperC25@users.noreply.github.com>
2019-07-05 10:50:16 +0800

    Add files via upload

---
+++ suijishu.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+# Author:zhenghanchao
+import random
+import string
+import sys
+def generate_random_str(randomlength=16):
+    """
+    生成一个指定长度的随机字符串，其中
+    string.digits=0123456789
+    string.ascii_letters=abcdefghigklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+    """
+    str_list = [random.choice(string.digits + string.ascii_letters) for i in range(randomlength)]
+    random_str = ''.join(str_list)
+    return random_str
+
+if __name__=='__main__':
+    print(generate_random_str(int(sys.argv[1])))
