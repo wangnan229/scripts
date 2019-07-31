0275a3625ff4a1e70bef619767d37bdc6206ec35
SuperC25 <43820420+SuperC25@users.noreply.github.com>
2019-07-31 16:02:21 +0800

    centos初始化脚本
    centos初始化脚本

--- centos_init.sh
+++ centos_init.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 # desc: CentOS initialization
-# curl https://yiyingcanfeng.github.io/centos-init.sh | bash
-# 可选参数base kernel python php nodejs cmd_game jdk mysql mongodb docker
-# 比如：curl https://yiyingcanfeng.github.io/centos-init.sh | bash -s base
 # Author: SuperC25
 # Mail: superchao25@gmail.com
 # Last Update: 2019.05.08
